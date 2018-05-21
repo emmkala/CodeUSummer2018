a61011e111f837b073cc4fad0566ca466aae6e75
peilinzhen <37724219+peilinz@users.noreply.github.com>
2018-05-21 18:48:05 -0400

    Create AdminServlet.java

---
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -0,0 +1,43 @@
+package codeu.controller;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.time.Instant;
+import codeu.model.data.Conversation;
+import codeu.model.data.User;
+import code.model.data.Message;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.UserStore;
+
+
+public class AdminServlet extends HttpServlet {
+    List<String> admin = Arrays.asList("anAdmin", "Admin1", "Admin2");
+    private UserStore userStore;
+    private ConversationStore conversationStore;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        int numOfConvo = conversationStore.getAllConversations().size();
+        request.setAttribute("numofConvo", numOfConvo);
+
+        String username = (String) request.getSession().getAttribute("user");
+        if (admin.contains(username)) {
+            //if(username.equals("anAdmin")){
+            response.sendRedirect("/admin");
+            return;
+        }
+
+    }
+}
